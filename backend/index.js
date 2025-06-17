50363aa95e4cbe04e7f6fca2416738153201fc11
Devops_github <harshwardhan.rawani@gmail.com>
2025-06-17 11:43:26 +0530

    cor

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8080',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
